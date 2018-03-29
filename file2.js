aed46ae87689b4fab826a39fdb9e961c2e250488
Andy Gaydos <gaydos@ucar.edu>
2018-03-29 15:18:11 -0600

    Reworked function in file #2

--- file2.js
+++ file2.js
@@ -3,6 +3,6 @@
 //////////////////////////////////////////////////////////
 
 function doThings(things) {
-    things.setStatus("TOO EXPENSIVE")
+    things.setStatus("UNWORKABLE PLAN")
     return false;
 }
